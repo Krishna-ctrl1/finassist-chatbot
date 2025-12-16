468d532949a25de9479bd664ea52a1e677b67fd3
Krishna-ctrl1 <143648025+Krishna-ctrl1@users.noreply.github.com>
2025-12-16 17:58:18 +0530

    fixed

--- backend/new_server.js
+++ backend/new_server.js
@@ -90,7 +90,7 @@ app.use("/api/auth", authLimiter);
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://127.0.0.1:5500"],
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
